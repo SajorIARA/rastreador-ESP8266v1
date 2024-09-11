f79deb35c054d76fc61de05584d4e402d540c3d9
SajorIARA <167906144+SajorIARA@users.noreply.github.com>
2024-09-11 17:25:26 +0000

    Mensaje de commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Web Simple</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenido a mi página web</h1>
+    </header>
+    <main>
+        <p>Esto es un párrafo en una página web simple.</p>
+    </main>
+    <footer>
+        <p>&copy; 2024 Mi Página Web</p>
+    </footer>
+</body>
+</html>
